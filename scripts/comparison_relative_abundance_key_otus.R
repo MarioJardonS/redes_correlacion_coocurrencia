ebc03e6d7e72bcf4b6343859b84058d4d525eb7a
MJardon913 <dantiam@ciencias.unam.mx>
2024-01-22 15:58:47 -0600

    edicion a script de figuras comparativas

--- scripts/comparison_relative_abundance_key_otus.R
+++ scripts/comparison_relative_abundance_key_otus.R
@@ -10,20 +10,41 @@ library(phyloseq)
 library(ggplot2)
 library(RColorBrewer)
 
-#construcción de phyloseqs desde argumentos 
 
-lista <- args[1]
-taxonomy <- paste0( "../data/taxonomy/" , args[2] )
-metadata <- paste0("../data/metadata/" , args[4] ) #los argumentos deberían de estar en la carpeta 
+
+#argumentos generales a todos los phyloseqs, comparten taxonomía, metadatos, y metadato elegido como tipo
+taxonomy <- paste0( "../data/taxonomy/" , args[1] )
+metadata <- paste0("../data/metadata/" , args[2] ) #los argumentos deberían de estar en la carpeta 
 #correspondiente
-tipo <- args[5] #nombre de la columna de metadatos a considerar
-nivel <- args[6]
+tipo <- args[3] #nombre de la columna de metadatos a considerar
 
-poco_abundante <- args[7]
+#nivel taxonómico al que se hará figura
+nivel <- args[4]
+#criterio para separar otus que se graficaran por separado y los demás
+poco_abundante <- args[5]
 
+#lista de tablas de otus a comparar´
+lista <- list(args[6:length(args)])
 
-
-
+##cada uno de los elementos de la lista se 
+lista_data <- list()
+for (i in 1:length(lista)){
+  
+  data <- read.csv( paste0("../data/tables/" , lista[[i]]) , row.names = 1 , header = TRUE )
+  
+  col <- c()
+  for (j in 1:dim(data)[2]) {
+    colj <- make.names(colnames(data)[j])
+    colj <- substr(colj  , 1 ,  nchar(colj)-21)
+    #print(colj)
+    col <- c(col , colj)
+    
+  }
+  
+  colnames(data) <- col
+  lista_data[[i]] <- data
+  
+}
 
 
 #figuras para abundancia relativa de filo, familia y género
@@ -41,9 +62,49 @@ if (nivel == "Phylum"){
   }
 }
 
-df_key <- tax_glom(phy_key, taxrank = nivel0)
-df_key <- psmelt(df_key)
-colnames(df_key)[dim(df_key)[2]] <- nivel
+##utilización de tipo para sacar samp_data desde metadata
+
+###los nombres de las muestras están en una columna llamada "ID"
+###los nombres de las muestras se corrigen según la sintaxis de R
+for (i in 1:dim(metadata)[1]){
+  metadata[i , "ID"] <- make.names(metadata[i , "ID"])
+}
+
+###se restringen los metadatos a las muestras usadas en el análisis de OTUs clave
+metadata <- metadata[which(is.element(metadata[ , "ID"], colnames(tabla) )) ,  ]
+
+tipo <- data.frame( ID = metadata[ , "ID"], Type = metadata [ , tipo ],row.names = metadata[ , "ID"])
+
+tipo <- sample_data(tipo)
+
+##lista en que se guardarán los phyloseq
+lista_phy <- list()
+
+for (i in 1:length(lista)){
+  o_table_i <- otu_table(lista_data[[i]][ intersect(row.names(lista_data[[i]]) , row.names(taxonomy)) ,  intersect(row.names(tipo) ,colnames(lista_data[[i]])) ], taxa_are_rows = TRUE)
+  
+  
+  taxonomy_i <- taxonomy[ intersect(row.names(taxonomy) ,  row.names(lista_data[[i]]) ) , ]
+  taxonomy_table <- tax_table(taxonomy_i)
+  row.names(taxonomy_table@.Data) <- row.names(taxonomy_i)
+  
+}
+
+lista_df <- list()
+
+for (i in 1:length(lista)){
+  
+  df_key <- tax_glom(lista_phy[[i]], taxrank = nivel0)
+  df_key <- psmelt(df_key)
+  colnames(df_key)[dim(df_key)[2]] <- nivel
+  lista_df[[i]] <- df_key
+}
+
+df <- lista_df[[1]]
+for (i in 1:(length(lista))){
+  df <- merge(lista_df[[i]] , df ,  all = TRUE)
+}
+
 
 ##poco abundante
 if (poco_abundante == "Si"){
